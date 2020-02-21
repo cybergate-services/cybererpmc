3c385ed1afb0ed9d338b4be7a81016fdee04fba5
DamithDDS <32153823+DamithDDS@users.noreply.github.com>
2020-02-21 16:46:41 +0530

    Update setup_customer.sh

--- setup_customer.sh
+++ setup_customer.sh
@@ -1,30 +1,16 @@
 #!/usr/bin/env bash
 
-if [ -f cybererp.conf ]; then
-  read -r -p "A config file exists and will be overwritten, are you sure you want to contine? [y/N] " response
-  case $response in
-    [yY][eE][sS]|[yY])
-      mv cybererp.conf cybererp.conf_backup
-      ;;
-    *)
-      exit 1
-    ;;
-  esac
-fi
-
-if [ -f ./.env ]; then
-  rm -f  ./.env
-fi
-
-echo "Press enter to confirm the detected value '[value]' where applicable or enter a custom value."
-while [ -z "${CYBERERP_HOSTNAME}" ]; do
-  read -p "Hostname (FQDN): " -e CYBERERP_HOSTNAME
-  DOTS=${CYBERERP_HOSTNAME//[^.]};
-  if [ ${#DOTS} -lt 2 ] && [ ! -z ${CYBERERP_HOSTNAME} ]; then
-    echo "${CYBERERP_HOSTNAME} is not a FQDN"
-    CYBERERP_HOSTNAME=
+echo "Enter Odoo customer name"
+while [ -z "${CUSTOMER_NAME}" ]; do
+  read -p "Odoo customer name: " -e CUSTOMER_NAME
+  if [ ! -z ${CUSTOMER_NAME} ]; then
+    echo "Customer name cannot be empty. Please re-enter it again"
+    CUSTOMER_NAME=
   fi
 done
+cp docker-compose-customer.yml docker-compose-${CUSTOMER_NAME}.yml
+sed -i "s/CUSTOMER/${CUSTOMER_NAME}/g" docker-compose-${CUSTOMER_NAME}.yml
+
 
 echo "Enter SMTP host that will be used to send mails from Odoo"
 while [ -z "${SMTP_HOST}" ]; do
@@ -72,62 +58,22 @@ while [ -z "${SMTP_PASSWORD}" ]; do
   fi
 done
 
-if [ -a /etc/timezone ]; then
-  DETECTED_TZ=$(cat /etc/timezone)
-elif [ -a /etc/localtime ]; then
-  DETECTED_TZ=$(readlink /etc/localtime|sed -n 's|^.*zoneinfo/||p')
-fi
-
-while [ -z "${CYBERERP_TZ}" ]; do
-  if [ -z "${CYBERERP_TZ}" ]; then
-    read -p "Timezone: " -e CYBERERP_TZ
-  else
-    read -p "Timezone [${DETECTED_TZ}]: " -e CYBERERP_TZ
-    [ -z "${CYBERERP_TZ}" ] && CYBERERP_TZ=${DETECTED_TZ}
-  fi
-done
-
 PASSWORD=$(LC_ALL=C </dev/urandom tr -dc A-Za-z0-9 | head -c 28)
-
-HTPASSWD=$(which htpasswd) 
-if [ -z "${HTPASSWD}" ]; then
-  echo "htpasswd command is not found and installing it now..."
-  sudo apt install -y apache2-utils
-  htpasswd -b -c ./conf/htpasswd admin ${PASSWORD}
-else 
-  htpasswd -b -c ./conf/htpasswd admin ${PASSWORD}
-fi
 
 POSTGRESQL_PASSWORD=${PASSWORD}
 ODOO_PASSWORD=${PASSWORD}
 ADMIN_PASSWORD=${PASSWORD}
-PUID=1002
-PGID=1002
-URL=$(echo ${CYBERERP_HOSTNAME} | cut -n -f 1  -d . --complement)
-SUBDOMAINS=odoo,odoo-pgadmin,odoo-portainer,odoo-backup
-EXTRA_DOMAINS=
-VALIDATION=http
 EMAIL=${ODOO_EMAIL}
 DHLEVEL=2048 
 ONLY_SUBDOMAINS=true 
 STAGING=false
 
-cat << EOF > cybererp.conf
-# -------------------------------------
-# CyberERP docker-compose Environment
-# -------------------------------------
-CYBERERP_HOSTNAME=${CYBERERP_HOSTNAME}
+cat << EOF > env _${CUSTOMER_NAME}.conf
 
 # ----------------------------------
 # POSTGRESQL Database Environment
 # ----------------------------------
 POSTGRESQL_PASSWORD=${PASSWORD}
-
-# --------------------
-# PGADMIN Environment
-# --------------------
-PGADMIN_PASSWORD=${PASSWORD}
-PGADMIN_EMAIL=${EMAIL}
 
 # -------------------
 # ODOO Environment
@@ -143,22 +89,9 @@ SMTP_PORT=465
 SMTP_USER=${SMTP_USER}
 SMTP_PASSWORD=${SMTP_PASSWORD}
 SMTP_PROTOCOL=ssl
-#-------------------------
-# LETSENCRYPT Environment
-#-------------------------
-PUID=${PUID}
-PGID=${PGID}
-URL=${URL}
-SUBDOMAINS=${SUBDOMAINS}
-EXTRA_DOMAINS=${EXTRA_DOMAINS}
-VALIDATION=${VALIDATION}
-EMAIL=${EMAIL}
-DHLEVEL=${DHLEVEL}
-ONLY_SUBDOMAINS=${ONLY_SUBDOMAINS}
-STAGING=${STAGING}
-TZ=${CYBERERP_TZ}
+
 EOF
 
-ln ./cybererp.conf ./.env
-mkdir -p ./postgresql_data
-chmod 1777 ./postgresql_data
+
+mkdir -p ./postgresql_${CUSTOMER_NAME}_data
+chmod 1777 ./postgresql_${CUSTOMER_NAME}_data
